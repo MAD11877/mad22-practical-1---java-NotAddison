e3f20089ffba362e0528d3fdd3317217d3e18aaa
NotAddison <S10222525@connect.np.edu.sg>
2022-04-20 17:09:28 +0800

    Update Question2.java - Completed Task

--- Question2.java
+++ Question2.java
@@ -11,6 +11,13 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter your height in meters: ");
     double height = in.nextDouble();
+    System.out.print("Enter your weight in kilograms: ");
+    double weight = in.nextDouble();
+
+    in.close(); // Close Scanner to prevenet Resource Leak
+    double bmi = weight / (height * height);
+    System.out.println("Your BMI is :" + bmi);
   }
 }
