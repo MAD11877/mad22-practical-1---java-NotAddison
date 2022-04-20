10f0f06ea0d7a1f4683237e11cd93f940fd0897a
NotAddison <S10222525@connect.np.edu.sg>
2022-04-20 17:10:27 +0800

    Update Question3.java - Completed Task

--- Question3.java
+++ Question3.java
@@ -11,6 +11,10 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter an Number To Multiply Itself: ");
+    int Number = in.nextInt();
+
+    in.close(); // Close Scanner to prevenet Resource Leak
+    System.out.println("Result: " + Number*Number);
   }
 }
