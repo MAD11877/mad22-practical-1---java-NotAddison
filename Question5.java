42b27de2d563bdb766c30e281452d1c0e7ab53d5
NotAddison <S10222525@connect.np.edu.sg>
2022-04-20 17:14:03 +0800

    Update Question5.java - Completed Task

--- Question5.java
+++ Question5.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList; // For ArrayLists
 import java.util.Scanner;
 
 public class Question5
@@ -27,6 +28,39 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    ArrayList<Integer> numbers = new ArrayList<>();
+    System.out.print("How many numbers do you want? ");
+    int repetion = in.nextInt();
+
+    // Get User Inputs (numbers)
+    for (int i = 0; i < repetion; i++) {
+      System.out.print("Type a number: ");
+      numbers.add(in.nextInt());
+    }
+    in.close(); // Close Scanner to prevenet Resource Leak (Inputs no longer required)
+
+    // Find Mode 
+    int highest = 0;
+    int mode = 0;
+
+    for (Integer integer : numbers) {
+      int count = 0;
+      for (Integer integer2 : numbers) {
+        if (integer == integer2) {
+          count++;
+        }
+      }
+
+      // Check if current count is greater than prev count
+      if (count > highest) {
+        highest = count;
+        mode = integer;
+      }
+      else if (count == highest){ // Bimodal Case
+        continue;
+      }
+    }
     
+    System.out.println("\nMode: " + mode);
   }
 }
